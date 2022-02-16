4f40cb99e3d20de9a260c768a27266d50bf240ce
John Andrews <john@reven.nz>
2022-02-16 22:23:13 +1300

    adding cache for live status

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,25 +6,57 @@ function abortRequests() {
     abortController.abort();
 }
 
-function LiveApp(name, instanceUid, interval) {
+function getFromLocalStorage(instaceUid){
+    try{
+        let item = JSON.parse(localStorage.getItem(instaceUid));
+        if(!item?.date)
+            return;
+        if(item.date < (new Date().getTime() - 60000))
+            return; // older than a minute reject it
+        return item.html;
+    }
+    catch(err) { return; }
+}
+
+function setInLocalStorage(instanceUid, html){
+    localStorage.setItem(instanceUid, JSON.stringify({
+        date: new Date().getTime(),
+        html: html
+    }));
+}
+
+function LiveApp(name, instanceUid, interval, subsequent) {
     if (!interval) interval = 3000;
+
+    if(!subsequent){
+        let html = getFromLocalStorage(instanceUid);
+        if(html) {
+            setStatus(name, instanceUid, interval, html);
+            return;
+        }
+    }
 
     fetch(`/apps/${encodeURIComponent(name)}/${encodeURIComponent(instanceUid)}/status?name=` + encodeURIComponent(name), {        
         signal: signal
-    })
-        .then(res => res.text())
-        .then(html => {
-        let ele = document.getElementById(instanceUid).querySelector('.status');
-        if (ele && html) {
-            ele.innerHTML = html;
-        }
-        setTimeout(() => LiveApp(name, instanceUid, interval), interval);
     }).catch(error => {
         console.log(name + ' error: ' + error);
-        setTimeout(() => LiveApp(name, instanceUid, interval), interval);
+        setTimeout(() => LiveApp(name, instanceUid, interval, true), interval + Math.random() + 5);
+    })
+    .then(res => res.text())
+    .then(html => {
+        setStatus(name, instanceUid, interval, html);
     });
-
 }
+
+function setStatus(name, instanceUid, interval, html){
+    let ele = document.getElementById(instanceUid).querySelector('.status');
+    if (ele && html) {
+        ele.innerHTML = html;
+        setInLocalStorage(instanceUid, html);
+    }
+    setTimeout(() => LiveApp(name, instanceUid, interval, true), interval + Math.random() + 5);
+}
+
 function htmlEncode(text) {
     var node = document.createTextNode(text);
     var p = document.createElement('p');
