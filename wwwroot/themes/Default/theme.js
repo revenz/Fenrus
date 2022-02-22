76aa80acebc76a5a361604f88aa4d4a921c43518
John Andrews <john@reven.nz>
2022-02-22 21:44:28 +1300

    more tweaks to default theme

--- wwwroot/themes/Default/theme.js
+++ wwwroot/themes/Default/theme.js
@@ -17,14 +17,39 @@ class DefaultTheme
 
 
     shrinkGroups(){
-        for(let grp of document.querySelectorAll('.db-group')){
-            this.shrinkGroup(grp);
+        let groups = document.querySelectorAll('.db-group');
+        for(let i=0;i<groups.length;i++)
+        {
+            this.shrinkGroup(groups[i], i == 0 ? null : groups[i-1], i == groups.length - 1 ? null : groups[i+1]);
         }
     }
-    shrinkGroup(grp){
+    shrinkGroup(grp, previous, next){
         let maxWidth = 0;
+
+        let grpBounds = grp.getBoundingClientRect();
+        let height = 0;
+        // see if there is a previous element, if so match that height, if not a next element, if so match that height
+        if(previous){
+            let prevBounds = previous.getBoundingClientRect();
+            if(prevBounds.y === grpBounds.y){
+                // make sure same row
+                height = prevBounds.height;
+            }
+        }
+        if(next){
+            let nextBounds = next.getBoundingClientRect();
+            if(nextBounds.y === grpBounds.y){
+                // make sure same row
+                height = Math.max(nextBounds.height, height);
+            }
+        }
+        if(height === 0){
+            height = grpBounds.height;
+        }
+        grp.style.height = height +'px';
+
         let offset = grp.getBoundingClientRect().x;
-        let height = grp.getBoundingClientRect().height;
+        
         for(let item of grp.querySelectorAll('.items .db-item')){
             let bounds = item.getBoundingClientRect();
             let width = bounds.x + bounds.width - offset;
@@ -33,7 +58,6 @@ class DefaultTheme
         }
         if(maxWidth > 0){
             grp.style.width = maxWidth + 'px';
-            grp.style.height = height +'px';
         }
     }
 }
