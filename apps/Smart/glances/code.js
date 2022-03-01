7ba766dac561a97adc2b7ffdb2638e0e88de8b74
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2022-03-01 22:28:59 +0000

    cleanup

--- apps/Smart/glances/code.js
+++ apps/Smart/glances/code.js
@@ -1,43 +1,34 @@
-﻿class Glances
-{
-	doFetch (args, endpoint) {
+class Glances {
+    doFetch(args, endpoint) {
         return args.fetch({
-            url: `api/3/` + endpoint, 
-			timeout: 10
+            url: `api/3/` + endpoint,
+            timeout: 10
         });
-    }  
-	
-    async status(args) {
-	 let firstQuery = args.properties['firstStatQuery'];
-     let secondQuery = args.properties['secStatQuery'];
-		
-	let data = await this.doFetch(args, firstQuery);
-		   
-    let firstQueryValue = firstQuery.split(/[/]+/).pop();
-	let firstQueryResult = data[firstQueryValue];
-	
-	console.log("firstQueryValue" , firstQueryValue);
-	console.log("firstQueryResult" , firstQueryResult);
-	console.log("firstQueryResult Data" , data);
-	
-	data = await this.doFetch(args, secondQuery);
-		   
-    let secondQueryValue = secondQuery.split(/[/]+/).pop(); 
-	let secondQueryResult = data[secondQueryValue]
+    }
 
-console.log("secondQueryValue" , secondQueryValue);
-	console.log("secondQueryResult" , secondQueryResult);
-	console.log("secondQueryResult Data" , data);
-	
-	let firstTitle = args.properties['firstStatTitle'];
-    let secondTitle = args.properties['secStatTitle'];
-		
+    async status(args) {
+        let firstQuery = args.properties['firstStatQuery'];
+        let secondQuery = args.properties['secStatQuery'];
+
+        let data = await this.doFetch(args, firstQuery);
+
+        let firstQueryValue = firstQuery.split(/[/]+/).pop();
+        let firstQueryResult = data[firstQueryValue];
+
+        data = await this.doFetch(args, secondQuery);
+
+        let secondQueryValue = secondQuery.split(/[/]+/).pop();
+        let secondQueryResult = data[secondQueryValue]
+
+        let firstTitle = args.properties['firstStatTitle'];
+        let secondTitle = args.properties['secStatTitle'];
+
         return args.liveStats([
             [firstTitle, firstQueryResult],
             [secondTitle, secondQueryResult]
         ]);
     }
-    
+
     async test(args) {
         let data = await this.doFetch(args, 'processcount/total');
         console.log('data', data);
