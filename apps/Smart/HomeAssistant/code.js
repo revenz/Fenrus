85b9dca4868e1b02eb78ebe3b55ac2331c61bc09
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2022-03-01 19:45:33 +0000

    Add files via upload

---
+++ apps/Smart/HomeAssistant/code.js
@@ -0,0 +1,51 @@
+﻿class HomeAssistant {
+    doFetch(args, template, isTest) {
+        if (isTest) {
+            return args.fetch({
+                url: `api/`,
+                timeout: 10,
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + args.properties['apikey']
+                }
+            });
+        } else {
+            return args.fetch({
+                url: `api/template`,
+                timeout: 10,
+                method: 'POST',
+                body: "{\"template\": \"" + template + "\"}",
+                headers: {
+                    "Accept": "text/html",
+                    'Authorization': 'Bearer ' + args.properties['apikey']
+                }
+            });
+        }
+    }
+    async status(args) {
+
+        let firstTemplate = args.properties['firstStatTemplate'];
+        let secondTemplate = args.properties['secStatTemplate'];
+
+        let firstTemplateResp = await this.doFetch(args, firstTemplate, false);
+        let secondTemplateResp = await this.doFetch(args, secondTemplate, false);
+
+        let firstTitle = args.properties['firstStatTitle'];
+        let secondTitle = args.properties['secStatTitle'];
+
+        let firstTemplateRespText = await firstTemplateResp?.text()
+        let secondTemplateRespText = await secondTemplateResp?.text()
+
+        return args.liveStats([
+            [firstTitle, firstTemplateRespText],
+            [secondTitle, secondTemplateRespText]
+        ]);
+    }
+
+    async test(args) {
+        let data = await this.doFetch(args, "", true);
+        return data?.message == 'API running.';
+    }
+}
+
+module.exports = HomeAssistant;
