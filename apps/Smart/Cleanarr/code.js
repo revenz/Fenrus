847b7f89a02dc3d95e1d953fddc828c3ae2e7dfa
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2022-04-21 14:52:51 +0100

    Add files via upload

---
+++ apps/Smart/Cleanarr/code.js
@@ -0,0 +1,33 @@
+﻿class Cleanarr {
+    doFetch(args, endpoint) {
+        return args.fetch({
+            url: endpoint,
+            timeout: 18000
+        });
+    }
+
+    async status(args) {
+        let data = await this.doFetch(args, 'content/dupes');
+
+        let count = data?.length;
+        let totalSizeOfDups = 0;
+
+        for (const fileData of data) {
+            totalSizeOfDups += fileData.media[1].parts[0].size;
+        }
+        console.log('Duplicate Count', count);
+        console.log('Size of Duplicates', args.Utils.formatBytes(totalSizeOfDups));
+        return args.liveStats([
+            ['Duplicate Count', count],
+            ['Size of Duplicates', args.Utils.formatBytes(totalSizeOfDups)]
+        ]);
+    }
+
+    async test(args) {
+        let data = await this.doFetch(args, 'server/info');
+        console.log("data", data);
+        return data != null && data.name != null && data.name.length > 0;
+    }
+}
+
+module.exports = Cleanarr;
