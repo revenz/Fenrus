e83b48b48a3651e057d76f111766fef022c6c4de
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2022-04-27 21:57:38 +0100

    warnings for radarr

--- apps/Smart/Radarr/code.js
+++ apps/Smart/Radarr/code.js
@@ -4,14 +4,29 @@
     }
 
     async status(args) {
-	let updateAvailable = await this.getStatusIndicator(args);
-	args.setStatusIndicator(updateAvailable ? 'Update' : '');
         let filter = args.properties['filters'];
 
-        let data = await args.fetch({
-            url: this.getUrl(args, 'movie'), 
-			timeout: 10000
-        });
+        let data = []
+        let queueData = []
+
+        if (args.properties['fetchWarnings'] == 'true') {
+            [data, queueData] = await Promise.all([
+                await args.fetch({
+                    url: this.getUrl(args, 'movie'),
+                    timeout: 10000
+                }),
+                await args.fetch({
+                    url: this.getUrl(args, 'queue') + '&pageSize=10000',
+                    timeout: 5000
+                })
+            ]);
+        } else {
+            data = await args.fetch({
+                url: this.getUrl(args, 'movie'),
+                timeout: 10000
+            });
+        }
+
         let filteredData = data?.filter((x, index, arr) => {
             let validEntry = x.hasFile == false;
             if (validEntry && (filter == 'both' || filter == 'available')) {
@@ -26,19 +41,29 @@
 
         data = await args.fetch(this.getUrl(args, 'queue'));
         let queue = data?.records?.length ?? 0;
-        return args.liveStats([
-            ['Missing', missingCount],
-            ['Queue', queue]
-        ]);
-    }
 
-    async getStatusIndicator(args){
-        let data = await args.fetch(this.getUrl(args, 'update'));
-		
-        if(data?.length > 0 && data[0].installed === false && data[0].latest === true)
-		    return 'update';
-    	return '';
-	}
+        if (args.properties['fetchWarnings'] != 'true') {
+            return args.liveStats([
+                ['Missing', missingCount],
+                ['Queue', queue]
+            ]);
+        } else {
+            let queueDataRecords = queueData?.records;
+            let filteredQueue = queueDataRecords?.filter((x, index, arr) => {
+                return x.trackedDownloadStatus == 'warning'
+            })
+            let totalWarnings = filteredQueue?.length ?? 0;
+
+            return args.liveStats([
+                ['Missing', missingCount],
+                ['Queue', queue],
+                ['Warnings', totalWarnings]
+            ]);
+        }
+
+
+
+    }
 
     async test(args) {
         let data = await args.fetch(this.getUrl(args, 'queue'));
