cb03b2ec015e1e47f13934cd697442528408504e
damda58 <dccebrian@gmail.com>
2022-04-27 14:17:05 +0200

    add update indicator
    add update indicator

--- apps/Smart/Flaresolverr/code.js
+++ apps/Smart/Flaresolverr/code.js
@@ -1,20 +1,37 @@
 class Flaresolverr {
 
-async status(args) {
-        const data = await args.fetch(args.url);
-        
-        if(!data?.version)
-                return args.liveStats([
-                        ['Version','Err'],
-                        ['Up','false'],
-                ]);
-        
-
-                return args.liveStats([
-                        ['Version',data.version],
-                        ['Up',data.msg == 'FlareSolverr is ready!'],
-                ]);
+        async getLastRelease(args){
+	        const data = await args.fetch({
+		        url: 'https://api.github.com/repos/FlareSolverr/FlareSolverr/releases/latest',
+		        timeout: 500
+	        });
+	        return data;
         }
+        async getStatus(args){
+		const data = await args.fetch(args.url);
+		return data;
+        }
+
+async status(args) {
+		const [ data, latest ] = await Promise.all([
+			await this.getStatus(args),
+			await this.getLastRelease(args)
+		]);
+		let indicator = '';
+		if(latest.tag_name != data.version){
+			indicator = 'update';
+		}
+		args.setStatusIndicator(indicator ? 'update' : '');
+		if(!data?.version)
+		return args.liveStats([
+			['Up','false']
+		]);
+		
+		
+		return args.liveStats([
+			['Up',data.msg == 'FlareSolverr is ready!'],
+		]);
+	}
         async test(args) {
                 const data = await args.fetch(args.url);
                 return data.msg == 'FlareSolverr is ready!';
