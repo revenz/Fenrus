c1035c3af981178df84df6b045371fada9c70be6
John Andrews <john@reven.nz>
2022-05-11 12:14:55 +1200

    adding try/catch around proxy

--- routes/ProxyRouter.js
+++ routes/ProxyRouter.js
@@ -28,55 +28,66 @@ class ProxyRouter extends FenrusRouter {
         
     async proxyResource(req, res)
     {
-        let system = System.getInstance();
-        if(!system.AllowGuest && req.isGuest)
-            return res.sendStatus(401);
-
-        let url = req.params.url.replace(/\-/g, '/');
-        url = new Utils().base64Decode(url);
-        if(!url)
-            return res.sendStatus(404);
-
-        let isHttps = url.toLowerCase().startsWith('https:');
-        var q = urlParse(url, true);
-        let options = {
-            host: q.hostname,
-            port: q.port || (isHttps ? 443 : 80),
-            path:  q.path,
-            timeout: 5000
-        };
-        (isHttps ? https : http).get( options, (data) => 
+        try
         {
-            try
-            {
-                //remove express headers
-                Object.keys(res.getHeaders()).forEach((headerName) => {
-                    res.removeHeader(headerName);
-                });
-            
-                // write status
-                res.status(data.statusCode);
-                // write headers
-                Object.keys(data.headers).forEach((headerName) => {
-                    res.setHeader(headerName, data.headers[headerName]);
-                });
+            let system = System.getInstance();
+            if(!system.AllowGuest && req.isGuest)
+                return res.sendStatus(401);
 
-                // cache it
-                if(res.statusCode >= 200 && res.statusCode <= 299)
-                    res.setHeader('Cache-Control', 'public, max-age=' + (31 * 24 * 60 * 60));                
-            
-                //Pipe data to response stream
-                data.pipe(res);      
-            }
-            catch(err) 
-            {                
-                console.log('error proxying object', err);
-                try{
-                    res.sendStatus(500);
+            let url = req.params.url.replace(/\-/g, '/');
+            url = new Utils().base64Decode(url);
+            if(!url)
+                return res.sendStatus(404);
+
+            let isHttps = url.toLowerCase().startsWith('https:');
+            var q = urlParse(url, true);
+            let options = {
+                host: q.hostname,
+                port: q.port || (isHttps ? 443 : 80),
+                path:  q.path,
+                timeout: 5000
+            };
+            (isHttps ? https : http).get( options, (data) => 
+            {
+                try
+                {
+                    //remove express headers
+                    Object.keys(res.getHeaders()).forEach((headerName) => {
+                        res.removeHeader(headerName);
+                    });
+                
+                    // write status
+                    res.status(data.statusCode);
+                    // write headers
+                    Object.keys(data.headers).forEach((headerName) => {
+                        res.setHeader(headerName, data.headers[headerName]);
+                    });
+
+                    // cache it
+                    if(res.statusCode >= 200 && res.statusCode <= 299)
+                        res.setHeader('Cache-Control', 'public, max-age=' + (31 * 24 * 60 * 60));                
+                
+                    //Pipe data to response stream
+                    data.pipe(res);      
                 }
-                catch(err2){} // can happen if closed
+                catch(err) 
+                {                
+                    console.log('error proxying object', err);
+                    try{
+                        res.sendStatus(500);
+                    }
+                    catch(err2){} // can happen if closed
+                }
+            });
+        }
+        catch(ex) 
+        {                
+            console.log('error proxying object', ex);
+            try{
+                res.sendStatus(500);
             }
-        });
+            catch(err2){} // can happen if closed
+        }
     }
 }
 
