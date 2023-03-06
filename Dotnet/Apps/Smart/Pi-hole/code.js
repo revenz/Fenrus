9217700d76100cbd77850b51a686bfc37d141d87
reven <john@reven.nz>
2023-03-06 17:44:03 +1300

    FR-149 - fixed pihole

--- Dotnet/Apps/Smart/Pi-hole/code.js
+++ Dotnet/Apps/Smart/Pi-hole/code.js
@@ -1,22 +1,27 @@
 ﻿class PiHole
 {
-    getUrl(url) {
+    getUrl(args) {
+        let url = args.url;
         if (!url)
             return '';
+        let token = args.properties['token'];
     
         if (url.endsWith('/'))
             url = url.substring(0, url.length - 1);
         if (url.endsWith('/admin') == false)
             url += '/admin';
-        url += '/api.php';
+        url += '/api.php/?summary&auth=' + token;
         return url;
     }
     
     async status(args) {
-        let url = this.getUrl(args.url);
+        let url = this.getUrl(args, args.url);
         let data = await args.fetch(url);
         let blocked = data?.ads_blocked_today ?? 0;
         let percent = data?.ads_percentage_today ?? 0;
+        
+        if(typeof(percent) === 'string')
+            percent = parseFloat(percent.replace(/,/g, ''), 10);
 
         return args.liveStats([
             ['Queries', blocked],
@@ -25,7 +30,7 @@
     }
 
     async test(args) {
-        let url = this.getUrl(args.url);
+        let url = this.getUrl(args);
 
         let data = await args.fetch(url);
         return isNaN(data?.ads_blocked_today) === false;
