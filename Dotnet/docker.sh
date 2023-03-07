9b38ad3e7dc0abff91bd8d60a52bedadebb932ba
reven <john@reven.nz>
2023-03-07 16:08:00 +1300

    tweaked docker.sh file to show clickable link

--- Dotnet/docker.sh
+++ Dotnet/docker.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 
 path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 dirData=$path/temp/Data
@@ -10,3 +10,5 @@ docker container stop fenrus  >/dev/null 2>&1
 docker container rm fenrus >/dev/null 2>&1
 docker build -t fenrus -f Dockerfile .
 docker run -d -p 5000:5000 -v $dirData:/App/Data -v $dirLogs:/App/Logs --restart unless-stopped --name fenrus fenrus
+
+echo -e '\e]8;;http://localhost:5000\a\033[0;32mCtrl+click to open Fenrus\033[0m\e]8;;\a'
