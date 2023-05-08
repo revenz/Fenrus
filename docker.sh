de0467e1b56099a8dc4e67916cf943fc8c577b4e
reven <john@reven.nz>
2023-05-08 17:32:25 +1200

    updated version and can now build arm64 docker

--- docker.sh
+++ docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# note: need to run the following to install dockerx for building both x64 and arm64
+# docker buildx install
+
 hyperlink(){ 
   printf '\033[0;32m'
   printf "$1"
@@ -8,10 +11,30 @@ hyperlink(){
   echo
 }
 
+cleanup() {
+  docker buildx rm fenrusbuilder >/dev/null 2>&1
+  exit  
+}
+
 git rev-list --count HEAD > gitversion.txt
 docker container stop fenrus  >/dev/null 2>&1
 docker container rm fenrus >/dev/null 2>&1
-docker build -t fenrus -f Dockerfile .
+docker buildx rm fenrusbuilder >/dev/null 2>&1
+
+docker buildx create --name fenrusbuilder --driver docker-container --platform linux/amd64,linux/arm64
+docker buildx use fenrusbuilder
+if [ "$1" = "--publish" ]; then
+  docker buildx build --push --platform linux/arm64,linux/amd64 -t revenz/fenrus:dotnet -f Dockerfile .
+  cleanup
+elif [ "$1" = "--dev" ]; then
+  docker buildx build --push --platform linux/arm64,linux/amd64 -t revenz/fenrus:develop -f Dockerfile .  
+  cleanup
+else
+  docker buildx build -t fenrus --platform linux/amd64 -f Dockerfile .
+fi
+
+docker buildx rm fenrusbuilder
+# docker build -t fenrus -f Dockerfile .
 
 if [ "$1" = "--publish" ]; then
   read -p "Do you want to publish to Dockerhub? (Y/N) " answer
@@ -25,7 +48,6 @@ if [ "$1" = "--publish" ]; then
   fi
 elif [ "$1" = "--dev" ]; then
   echo Publishing develop docker image
-  docker tag fenrus revenz/fenrus:develop
   docker push revenz/fenrus:develop
 else
   echo Running docker image
