c6c493f9e005310ad364d3d057e74aa11d9c8ef0
Owen Worley <owen@owenworley.co.uk>
2025-11-18 18:47:31 +0000

    Refactor Tdarr status method for pie stats API
    The previous API method used to talk to tdarr was changed.
    I have changed it here to use the new get-pies api.

--- Apps/Smart/tdarr/code.js
+++ Apps/Smart/tdarr/code.js
@@ -1,56 +1,55 @@
 class Tdarr {
     status(args) {
-		
-		
-      let url = args.url;
-      if(url.endsWith('/'))
-        url = url.substring(0, url.length - 1);
-	
-      const data = args.fetch(
-        {
-          url: `${url}/api/v2/cruddb/`,
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-          },
-          body: JSON.stringify({
-            data: {
-              collection: 'StatisticsJSONDB',
-              mode: 'getById',
-              docID: 'statistics',
-            },
-          }),
-        },
-      ).data;
-  
-      if (!data) {
-        throw 'no data';
-      }
-  
-      const queue = parseInt(data.table1Count) + parseInt(data.table4Count);
-      const processed = parseInt(data.table2Count) + parseInt(data.table5Count);
-      const errored = parseInt(data.table3Count) + parseInt(data.table6Count);
 
-	  let results =	[['Queue', queue],
-        ['Proc / Err', `${processed} / ${errored}`]] 
-		
-	  if(args.properties['showSpaceSaved'] == 'true') {	
-	    const sizeSaved = args.Utils.formatBytes(parseFloat(data.sizeDiff) * 1000000000)
-		results.push(['Space saved', sizeSaved])
-	  }
-	  
-      return args.liveStats(results);
+
+        let url = args.url;
+        if (url.endsWith('/'))
+            url = url.substring(0, url.length - 1);
+
+        const data = args.fetch(
+            {
+                url: `${url}/api/v2/stats/get-pies`,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+                body: JSON.stringify({
+                    data: {
+                        libraryId: ''
+                    }
+                }),
+            },
+        ).data;
+
+        if (!data) {
+            throw 'no data';
+        }
+
+        var pieStats = data.pieStats;
+        if (!pieStats) {
+            throw 'no piestats';
+        }
+        const processed = pieStats.totalTranscodeCount;
+        const total = pieStats.totalFiles;
+
+        let results = [
+            ['Proc / Total', `${processed} / ${total}`]]
+
+        if (args.properties['showSpaceSaved'] == 'true') {
+            const sizeSaved = args.Utils.formatBytes(pieStats.sizeDiff * 1000000000)
+            results.push(['Space saved', sizeSaved])
+        }
+
+        return args.liveStats(results);
     }
-  
+
     test(args) {
-      let url = args.url;
-      if(url.endsWith('/'))
-        url = url.substring(0, url.length - 1);
-      const data = args.fetch(`${url}/api/v2/status`).data;
-      return (data.status === 'good');
+        let url = args.url;
+        if (url.endsWith('/'))
+            url = url.substring(0, url.length - 1);
+        const data = args.fetch(`${url}/api/v2/status`).data;
+        return (data.status === 'good');
     }
-  }
-  
-  module.exports = Tdarr;
-  
+}
+
